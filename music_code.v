0efe403006a5db32c44f0254432b456f46cf3068
EnigMoiD <evan.dorsky@icloud.com>
2013-12-12 12:36:06 -0500

    Switch 0 changes frequency

--- music_code.v
+++ music_code.v
@@ -1,15 +1,26 @@
 //http://www.fpga4fun.com/MusicBox2.html
-module music_code(clk, speaker);
-output speaker;
-input clk;
+module music_code(
+	input sw0,
+	input clk,
+	output speaker
+);
 
 reg flipper;
 assign speaker = flipper;
 
 reg [14:0] counter;
 
-always @(posedge clk) if(counter==28408) counter <= 0; else counter <= counter+1;
-
-always @(posedge clk) if(counter==28408) flipper <= ~flipper;
+always @(posedge clk)
+	if (sw0) begin
+		if(counter==28408) begin
+			counter <= 0;
+			flipper <= ~flipper;
+		end else counter <= counter+1;
+	end else begin
+		if(counter==20408) begin
+			counter <= 0;
+			flipper <= ~flipper;
+		end else counter <= counter+1;
+	end
 
 endmodule
