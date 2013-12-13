cbedf71a270b5a0207f464a77dfe1bab99f42eb2
EnigMoiD <evan.dorsky@icloud.com>
2013-12-13 18:03:31 -0500

    Two notes at the same time

--- make_song.v
+++ make_song.v
@@ -17,27 +17,14 @@ always @(posedge clk)
 		end else counter <= counter+1;
 		end
 
-
 endmodule
 
 module song(
   input sw0, 
   input clk, 
-  output speaker
+  output [1:0] speaker
 ); 
 
-reg [14:0] enable
-reg [14:0] counter_time 
-music_code(clk, speaker, 28408, enable[0]);
-music_code(clk, speaker, 20408, enable[1]);
-
-counter_time = 0
-enable = B1
-always @(posedge clk) begin
-  if counter ==(SOMETHING) begin
-    enable = enable >> 1;
-    counter_time = 0;
-  end
-else counter_time = counter_time + 1;
-  
+music_code c1(clk, speaker[0], 28408);
+music_code c2(clk, speaker[1], 20408);
 endmodule
