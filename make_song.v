aad67e74e98a6db6a4afa3d81b24948fd26bea92
jamiesarahg <jamie.gorson@students.olin.edu>
2013-12-13 17:58:56 -0500

    played multiple notes

--- make_song.v
+++ make_song.v
@@ -1,7 +1,8 @@
 module music_code(
 	input clk,
 	output speaker,
-	input value);
+	input value
+	input enable);
 
 reg flipper;
 assign speaker = flipper;
@@ -9,10 +10,12 @@ assign speaker = flipper;
 reg [14:0] counter;
 
 always @(posedge clk)
+  if (enable = 1) begin
 		if(counter==value) begin
 			counter <= 0;
 			flipper <= ~flipper;
 		end else counter <= counter+1;
+		end
 
 
 endmodule
@@ -22,6 +25,19 @@ module song(
   input clk, 
   output speaker
 ); 
-music_code(clk, speaker, 28408);
-music_code(clk, speaker, 20408);
+
+reg [14:0] enable
+reg [14:0] counter_time
+music_code(clk, speaker, 28408, enable[0]);
+music_code(clk, speaker, 20408, enable[1]);
+
+counter_time = 0
+enable = B1
+always @(posedge clk) begin
+  if counter ==(SOMETHING) begin
+    enable = enable >> 1;
+    counter_time = 0;
+  end
+else counter_time = counter_time + 1
+  
 endmodule
