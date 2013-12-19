9bc804edb3cc026250b0bcfa47a0a91bc5b11e6d
Rahil Dedhia <rahil.dedhia@students.olin.edu>
2013-12-18 22:49:50 -0500

    changed mem file name and LUT reg name and frequency divider on LUT_song.v

--- LUT_song.v
+++ LUT_song.v
@@ -5,17 +5,17 @@ module LUT_song(
 );
 
 	reg [31:0] counter_time; 
-	reg [6:0] LUT [0:47];
+	reg [6:0] LUT_song [0:47];
 	reg [31:0] note_index;
 
-	initial $readmemb("merrychristmas.mem", LUT);
+	initial $readmemb("merrychristmas.mem", LUT_song);
 
 	always @(posedge clk) begin
-		if (counter_time == 10000000000) begin
+		if (counter_time == 100000000) begin
 			note_index <= note_index + 1;
 			counter_time <= 0;
 		end else counter_time <= counter_time + 1;
 	end
 
-	assign note = LUT[note_index];
+	assign note = LUT_song[note_index];
 endmodule
